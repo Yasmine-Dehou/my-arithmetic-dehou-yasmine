ca9712b74af8c4f57fa329882463060fd760fb00
Yasmine Dehou <yasmine.dehou@gmail.com>
2024-11-26 18:01:48 +0100

    feat: amélioration de la documentation et ajout de fichiers de tests

---
+++ docs/source/modules.rst
@@ -0,0 +1,7 @@
+my-arithmetic-dehou-yasmine
+===========================
+
+.. toctree::
+   :maxdepth: 4
+
+   tests
--- src/my_arithmetic_dehou_yasmine/pgcd.py
+++ src/my_arithmetic_dehou_yasmine/pgcd.py
@@ -1,5 +1,19 @@
 def pgcd(a, b):
-    """Calcul du pgcd de a et b"""
+    """
+    Compute the greatest common divisor (GCD) of two numbers.
+
+    Parameters
+    ----------
+    a : int
+        First number.
+    b : int
+        Second number.
+
+    Returns
+    -------
+    int
+        The greatest common divisor of a and b.
+    """
     while b:
         a, b = b, a % b
     return a
